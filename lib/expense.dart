4869bdd4e3e0a087a7597aa565ec514e44a62101
andreyscott <andreyscott301@gmail.com>
2023-10-30 11:44:07 +0100

    ded the started file

---
+++ lib/expense.dart
@@ -0,0 +1,13 @@
+class Expense {
+  Expense({
+    required this .title,
+    required this.amount,
+    required this.date,
+  }) : id = ;
+
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+}
