c8507ed455c86aff5be809cce8c4430433b684f5
andreyscott <andreyscott301@gmail.com>
2023-10-30 13:31:54 +0100

    mage it sound

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_app/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,6 +11,12 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+ final List <Expense> _registeredExpenses = [
+  Expense(title: 'Flutter Course now', amount: 69.247, date: DateTime.now(), category: Category.work),
+
+  Expense(title: 'Cinema', amount: 79, date: DateTime.now(), category: Category.travel)
+ ]
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
