bdaec7a1c255cd0a0c151e9edd60f62cd882a89d
andreyscott <andreyscott301@gmail.com>
2024-01-10 20:16:55 +0100

    improve functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'package:expense_app/expenses.dart';
+import 'package:expense_app/widgets/expenses.dart';
+
 
 void main() {
   runApp(
     const MaterialApp(
-      home: ...,
+      home: Expenses(),
     ),
   );
 }
